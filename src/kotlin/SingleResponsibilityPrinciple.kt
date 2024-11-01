801336de32621c37911a66e39f3b15bd8f6ad795
Mohi <mohifayden@gmail.com>
2024-11-01 15:17:30 -0700

    Single Responsibility Principle (SRP)

---
+++ src/kotlin/SingleResponsibilityPrinciple.kt
@@ -0,0 +1,21 @@
+@file:Suppress("unused", "UNUSED_PARAMETER")
+
+package kotlin
+
+class AuthService {
+    fun authenticateUser(username: String, password: String) {
+        // Code to authenticate user
+    }
+}
+
+class EmailService {
+    fun sendEmail(message: String) {
+        // Code to send email
+    }
+}
+
+class ActivityLogger {
+    fun logActivity(activity: String) {
+        // Code to log activity
+    }
+}
---
+++ src/python/single_responsibility_principle.py
@@ -0,0 +1,16 @@
+class AuthService:
+    def authenticate_user(self, username, password):
+        # Code to authenticate user
+        pass
+
+
+class EmailService:
+    def send_email(self, message):
+        # Code to send email
+        pass
+
+
+class ActivityLogger:
+    def log_activity(self, activity):
+        # Code to log activity
+        pass
